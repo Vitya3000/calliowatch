9c518ffda7a363f777b4ad078e69f4d656160e86
Vitya3000 <109411554+Vitya3000@users.noreply.github.com>
2023-04-26 20:51:00 +0200

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,27 +2,16 @@
 let msg ='';
 //convertiertes string
 let rcvmsg='';
-
 //var fuer modus
 let mode =0;
-
-
 //customhintergrund
 let cust = 0;
-
-
-
 radio.setGroup(177);
 basic.forever(function() {
-
-
 //MODSSCHALTER------------------------------------------
     input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function() {
         mode = 0;
         rcvmsg = '';
-        
-      
-      
     })
     input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
         rcvmsg = '';
@@ -32,42 +21,30 @@ basic.forever(function() {
         rcvmsg = '';
         mode = 2;
     })
-
-    
    //MainMenu
 if(mode ==0){
-
         //hintergründe
         if(storage.getNumber(StorageSlots.s1)==0){
-
             basic.showIcon(IconNames.Heart);
         }
     if (storage.getNumber(StorageSlots.s1) == 1) {
-
         basic.showIcon(IconNames.Square);
     }
     if (storage.getNumber(StorageSlots.s1) == 2) {
-
         basic.showIcon(IconNames.Diamond);
     }
     if (storage.getNumber(StorageSlots.s1) == 3) {
-
         basic.showIcon(IconNames.Cow);
     }
     if (storage.getNumber(StorageSlots.s1) == 4) {
-
         basic.showIcon(IconNames.Rabbit);
     }
     if (storage.getNumber(StorageSlots.s1) == 5) {
-
         basic.showIcon(IconNames.Triangle);
     }
     if (storage.getNumber(StorageSlots.s1) == 6) {
-
         basic.showIcon(IconNames.SmallSquare);
     }
-
-   
     if(input.buttonIsPressed(Button.A)&& cust<6){
         cust++;
        
@@ -75,28 +52,13 @@ if(mode ==0){
     }
     if(input.buttonIsPressed(Button.B) && cust >0){
         cust--;
-        
         storage.putNumber(StorageSlots.s1, cust);
     }
 
-    
-   
-
-
-
 }
 
-
-
-
-
-
-
-
 //morsecode
-if(mode==1){ 
-   
-       
+if(mode==1){     
     
     morse();
     reciever();
@@ -117,15 +79,9 @@ if(mode==2){
         music.playMelody("E6 D6 F#5 - G#5 - C#6 B5 D5 - E5 - B5 A5 C#5 - E5 - A5", 400);
     }
     
-    
     basic.showIcon(IconNames.EigthNote);
-    
+
 }
-
-
-
-
-
 
 })
 
@@ -145,125 +101,17 @@ function morse(){
         msg = '';
     })
 }
-
-
 //EMPFÄNGERFUNKTION----------------
 function reciever(){ 
-    
     radio.onReceivedString(function(receivedString: string) {
         
-       if(receivedString[0] =='.'){
-
-                if(receivedString == morsecode[0]){
-
-                     rcvmsg +='A';
-                 }
-           if (receivedString == morsecode[4]) {
-
-               rcvmsg += 'E';
-           }
-           if (receivedString == morsecode[5]) {
-
-               rcvmsg += 'F';
-           }
-           if (receivedString == morsecode[7]) {
-
-               rcvmsg += 'H';
-           }
-           if (receivedString == morsecode[8]) {
-
-               rcvmsg += 'I';
-           }
-           if (receivedString == morsecode[9]) {
-
-               rcvmsg += 'J';
-           } if (receivedString == morsecode[11]) {
-
-               rcvmsg += 'L';
-           }
-           if (receivedString == morsecode[15]) {
-
-               rcvmsg += 'P';
-           }
-           if (receivedString == morsecode[17]) {
-
-               rcvmsg += 'R';
-           }
-           if (receivedString == morsecode[18]) {
-
-               rcvmsg += 'S';
-           }
-           if (receivedString == morsecode[20]) {
-
-               rcvmsg += 'U';
-           }
-           if (receivedString == morsecode[21]) {
-
-               rcvmsg += 'V';
-           }
-           if (receivedString == morsecode[22]) {
-
-               rcvmsg += 'W';
-           }
-       }
-          
-       else{
-               if (receivedString == morsecode[1]) {
-
-                   rcvmsg += 'B';
-               }
-               if (receivedString == morsecode[2]) {
-
-                   rcvmsg += 'C';
-               }
-               if (receivedString == morsecode[3]) {
-
-                   rcvmsg += 'D';
-               }
-               if (receivedString == morsecode[6]) {
-
-                   rcvmsg += 'G';
-               }
-               if (receivedString == morsecode[10]) {
-
-                   rcvmsg += 'K';
-               }
-               if (receivedString == morsecode[12]) {
-
-                   rcvmsg += 'M';
-               }
-               if (receivedString == morsecode[13]) {
-
-                   rcvmsg += 'N';
-               }
-               if (receivedString == morsecode[14]) {
-
-                   rcvmsg += 'O';
-               }
-               if (receivedString == morsecode[16]) {
-
-                   rcvmsg += 'Q';
-               }
-           if (receivedString == morsecode[19]) {
-
-               rcvmsg += 'T';
-           }
-            if (receivedString == morsecode[23]) {
-
-               rcvmsg += 'X';
-           } 
-           if (receivedString == morsecode[24]) {
-
-               rcvmsg += 'Y';
-           }
-           if (receivedString == morsecode[25]) {
-
-               rcvmsg += 'Z';
+       for(let i=0;i<26;i++){
+           if(receivedString == morsecode[i]){
+               rcvmsg +=alphabet[i];
            }
        }
     })
 }
-
 //MORSEALPHABET----------------------------
 let morsecode = [
     '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....',
@@ -271,3 +119,4 @@ let morsecode = [
     '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-',
     '-.--', '--..'
 ];
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
